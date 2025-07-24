6e98839963b89c1b1c78e2bd64a6fcf3ba555ef2
archit-asawa <architasawa2@gmail.com>
2025-07-24 14:30:14 +0530

    Update db.js

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'localhost',          // ✅ Correct from pgAdmin
-    port: 5432,                 // ✅ Correct from pgAdmin  
-    user: 'postgres',           // ✅ Correct from pgAdmin
-    password: 'architiiitn',  // Replace with your actual password
-    database: 'stockflow_db'    // We'll create this database
+    host: 'localhost',          
+    port: 5432,                 
+    user: 'postgres',       
+    password: 'xxxxx',  
+    database: 'stockflow_db' 
   },
   pool: {
     min: 2,
